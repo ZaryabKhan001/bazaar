7fa2b735cb28585400b734e5325f89dd457f1899
Zaryab Zubair <zaryabz043@gmail.com>
2025-11-17 17:05:26 +0500

    started auth controllers

---
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { validateRegistrationData } from '../utils/auth.utils';
+import prisma from '../../../../packages/libs/prisma';
+import { AuthError, ValidationError } from '../../../../packages/error-handler/index';
+
+export const handleUserRegistration = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    validateRegistrationData(req.body, 'user');
+    const { email } = req.body;
+
+    const isUserAlreadyExists = await prisma.users.findUnique({ where: email });
+
+    if (isUserAlreadyExists) {
+      return next(new ValidationError('User Already Exists with this Email!'));
+    }
+
+    // check otp
+
+    res.status(201).json();
+  } catch (error) {
+    console.log('Error in User Registration', error);
+    return next(new AuthError(500, 'Error in User Registration'));
+  }
+};

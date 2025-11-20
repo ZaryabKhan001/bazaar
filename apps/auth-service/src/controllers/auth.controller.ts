dc039913a673dd7b52a7808abc4a86d3e33dce5c
Zaryab Zubair <zaryabz043@gmail.com>
2025-11-20 18:01:52 +0500

    verify otp functonality done

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -1,7 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
-import { validateRegistrationData, checkOtpRestrictions, trackOTPRequests, sendOtp } from '../utils/auth.utils';
+import {
+  validateRegistrationData,
+  checkOtpRestrictions,
+  trackOTPRequests,
+  sendOtp,
+  verifyOTP,
+} from '../utils/auth.utils';
 import prisma from '../../../../packages/libs/prisma';
 import { ValidationError } from '../../../../packages/error-handler/index';
+import bcrypt from 'bcryptjs';
 
 export const handleUserRegistration = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -26,6 +33,35 @@ export const handleUserRegistration = async (req: Request, res: Response, next: 
     return res.status(201).json({
       success: true,
       message: 'OTP sent to your Email. Please verify your Account.',
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const handleVerifyOTP = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password, otp, name } = req.body;
+    if (!otp) {
+      return next(new ValidationError('OTP is required.'));
+    }
+
+    const user = await prisma.users.findUnique({ where: { email } });
+
+    if (!user) {
+      return next(new ValidationError('User not found'));
+    }
+
+    // verify otp
+    await verifyOTP(email, otp, next);
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.users.create({ data: { name, email, password: hashedPassword } });
+
+    return res.status(200).json({
+      success: true,
+      message: 'User registered successfully.',
     });
   } catch (error) {
     return next(error);
--- apps/auth-service/src/utils/constants.ts
+++ apps/auth-service/src/utils/constants.ts
@@ -1,7 +1,8 @@
 export const redis_keys = {
-    otp: 'otp:',
-    otp_lock: 'otp_lock:',
-    otp_spam_lock: 'otp_spam_lock:',
-    otp_cooldown: 'otp_cooldown:',
-    otp_request_count: 'otp_request_count:'
+  otp: 'otp:',
+  otp_lock: 'otp_lock:',
+  otp_spam_lock: 'otp_spam_lock:',
+  otp_cooldown: 'otp_cooldown:',
+  otp_request_count: 'otp_request_count:',
+  otp_attempts: 'otp_attempts:',
 } as const;
